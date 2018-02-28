405f74c32aad3763d4d12e227b16a2c2b3c2ccda
Li-Xin <mzydj@139.com>
2018-02-28 16:56:11 +0800

    refactor: Book和ListBooks组件改为无状态组件

--- src/Book.js
+++ src/Book.js
@@ -1,35 +1,33 @@
-import React, { Component }  from 'react';
+import React from 'react';
 
-class Book extends Component {
-	render() {
-		return (
-			<li>
-			  <div className="book">
-			    <div className="book-top">
-			      <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${this.props.book.imageLinks && this.props.book.imageLinks.thumbnail})` }}></div>
-			      <div className="book-shelf-changer">
-			        <select 
-			        	id={this.props.book.id}
-			        	value={this.props.book.shelf || 'none'/* 匹配下拉菜单默认选项 */}
-			        	onChange={(event) => {
-			        		// 判断本书是否在书架之上，如果在进行修改操作，不在进行添加操作
-			        		this.props.book.shelf?this.props.onUpdateBook(this.props.book, event.target.value):this.props.onAddBook(this.props.book, event.target.value);
-			        	}}
-			        >
-			          <option value="none" disabled>移动到</option>
-			          <option value="currentlyReading">正在阅读</option>
-			          <option value="wantToRead">想读</option>
-			          <option value="read">读完</option>
-			          <option value="none">移除</option>
-			        </select>
-			      </div>
-			    </div>
-			    <div className="book-title">{this.props.book.title}</div>
-			    <div className="book-authors">{this.props.book.authors}</div>
-			  </div>
-			</li>
-		)
-	}
+function Book(props) {
+	return (
+		<li>
+		  <div className="book">
+		    <div className="book-top">
+		      <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${props.book.imageLinks && props.book.imageLinks.thumbnail})` }}></div>
+		      <div className="book-shelf-changer">
+		        <select 
+		        	id={props.book.id}
+		        	value={props.book.shelf || 'none'/* 匹配下拉菜单默认选项 */}
+		        	onChange={(event) => {
+		        		// 判断本书是否在书架之上，如果在进行修改操作，不在进行添加操作
+		        		props.book.shelf?props.onUpdateBook(props.book, event.target.value):props.onAddBook(props.book, event.target.value);
+		        	}}
+		        >
+		          <option value="none" disabled>移动到</option>
+		          <option value="currentlyReading">正在阅读</option>
+		          <option value="wantToRead">想读</option>
+		          <option value="read">读完</option>
+		          <option value="none">移除</option>
+		        </select>
+		      </div>
+		    </div>
+		    <div className="book-title">{props.book.title}</div>
+		    <div className="book-authors">{props.book.authors}</div>
+		  </div>
+		</li>
+	)
 }
 
 export default Book
--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,61 +1,59 @@
-import React, { Component }  from 'react';
+import React from 'react';
 import Book from './Book';
 import Loading from './Loading';
 import { Link } from 'react-router-dom';
 
-class ListBooks extends Component {
-	render() {
-		return (
-			<div className="list-books">
-			  <div className="list-books-title">
-			    <h1>我的书架</h1>
-			  </div>
-			  <div className="list-books-content">
-			    <div>
-			      <div className="bookshelf">
-			        <h2 className="bookshelf-title">正在阅读</h2>
-			        <div className="bookshelf-books">
-			          {this.props.loaded || <Loading />}
-			          <ol className="books-grid">
-			          	{/* 在书架中所有书籍中筛选出本暑假图书进行渲染*/}
-			            {this.props.books.filter(book => (book.shelf === 'currentlyReading')).map(book => (
-							<Book book={book} key={book.id} onUpdateBook={this.props.onUpdateBook} />
-			            ))}
-			          </ol>
-			        </div>
-			      </div>
-			      <div className="bookshelf">
-			        <h2 className="bookshelf-title">想读</h2>
-			        <div className="bookshelf-books">
-			          {this.props.loaded || <Loading />}
-			          <ol className="books-grid">
-			          	{/* 在书架中所有书籍中筛选出本暑假图书进行渲染*/}
-			            {this.props.books.filter(book => (book.shelf === 'wantToRead')).map(book => (
-							<Book book={book} key={book.id} onUpdateBook={this.props.onUpdateBook} />
-			            ))}
-			          </ol>
-			        </div>
-			      </div>
-			      <div className="bookshelf">
-			        <h2 className="bookshelf-title">读完</h2>
-			        <div className="bookshelf-books">
-			          {this.props.loaded || <Loading />}
-			          <ol className="books-grid">
-			          	{/* 在书架中所有书籍中筛选出本暑假图书进行渲染*/}
-			          	{this.props.books.filter(book => (book.shelf === 'read')).map(book => (
-							<Book book={book} key={book.id} onUpdateBook={this.props.onUpdateBook} />
-			            ))}
-			          </ol>
-			        </div>
-			      </div>
-			    </div>
-			  </div>
-			  <div className="open-search">
-			    <Link to='/search'>添加一本书</Link>
-			  </div>
-			</div>
-		)
-	}
+function ListBooks(props) {
+	return (
+		<div className="list-books">
+		  <div className="list-books-title">
+		    <h1>我的书架</h1>
+		  </div>
+		  <div className="list-books-content">
+		    <div>
+		      <div className="bookshelf">
+		        <h2 className="bookshelf-title">正在阅读</h2>
+		        <div className="bookshelf-books">
+		          {props.loaded || <Loading />}
+		          <ol className="books-grid">
+		          	{/* 在书架中所有书籍中筛选出本暑假图书进行渲染*/}
+		            {props.books.filter(book => (book.shelf === 'currentlyReading')).map(book => (
+						<Book book={book} key={book.id} onUpdateBook={props.onUpdateBook} />
+		            ))}
+		          </ol>
+		        </div>
+		      </div>
+		      <div className="bookshelf">
+		        <h2 className="bookshelf-title">想读</h2>
+		        <div className="bookshelf-books">
+		          {props.loaded || <Loading />}
+		          <ol className="books-grid">
+		          	{/* 在书架中所有书籍中筛选出本暑假图书进行渲染*/}
+		            {props.books.filter(book => (book.shelf === 'wantToRead')).map(book => (
+						<Book book={book} key={book.id} onUpdateBook={props.onUpdateBook} />
+		            ))}
+		          </ol>
+		        </div>
+		      </div>
+		      <div className="bookshelf">
+		        <h2 className="bookshelf-title">读完</h2>
+		        <div className="bookshelf-books">
+		          {props.loaded || <Loading />}
+		          <ol className="books-grid">
+		          	{/* 在书架中所有书籍中筛选出本暑假图书进行渲染*/}
+		          	{props.books.filter(book => (book.shelf === 'read')).map(book => (
+						<Book book={book} key={book.id} onUpdateBook={props.onUpdateBook} />
+		            ))}
+		          </ol>
+		        </div>
+		      </div>
+		    </div>
+		  </div>
+		  <div className="open-search">
+		    <Link to='/search'>添加一本书</Link>
+		  </div>
+		</div>
+	)
 }
 	
 export default ListBooks
